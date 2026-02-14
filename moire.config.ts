13995ddb3a70cd6658ed36597e058a260d2191fb
HuIn2479 <66163050+HuIn2479@users.noreply.github.com>
2026-02-14 21:56:37 +0800

    Chore: update the title, author, and description in the configuration file

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
+  title: "夢澪の工房日常",
+  author: "EkiShion",
   theme: "classic",
   pageSize: 20,
   utcOffset: 8,
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "平安喜樂，萬事勝意，祝你，祝我，祝我們",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
-  url: "https://moire.blog"
+  url: "https://n.eki.ink"
 };
